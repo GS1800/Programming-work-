885905e789144486c9804d4fd2a1944e7bf1e2e4
GS1800 <Singh_is_king01@icloud.com>
2019-04-24 09:10:57 -0400

    update

--- Project/src/com/company/Player.java
+++ Project/src/com/company/Player.java
@@ -1,8 +1,28 @@
 package com.company;
 
+import java.util.Random;
+
 /**
  * Created by Gs1800 on 4/23/2019.
  */
 public class Player {
-    //hi
+
+    private int health;
+    private int defence;
+    private int attack;
+    private int playerlevel;
+
+    public int getCoin() {
+        return coin;
+    }
+
+    private int coin;
+
+    Random rand;
+
+    public Player() {
+        rand = new Random();
+        this.playerlevel = 1;
+        health = 100;
+    }
 }
